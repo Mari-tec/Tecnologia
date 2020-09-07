b7b5f1687d009efb1c6447b68c955ac742cc7d55
Mari-tec <70905102+Mari-tec@users.noreply.github.com>
2020-09-07 11:03:03 -0500

    Add files via upload

---
+++ Finalt.js
@@ -0,0 +1,8 @@
+document.getElementById("q").innerHTML = "TRIVIA - game";
+document.getElementById("w").innerHTML = "Instrucciones:";
+document.getElementById("e").innerHTML = "Al ingresar te mostrara 8 preguntas, una por una ";
+document.getElementById("r").innerHTML = "donde debes escoger entre a b c d.";
+document.getElementById("t").innerHTML = "";
+document.getElementById("y").innerHTML = "Tienes 3 vidas.";
+document.getElementById("u").innerHTML = "Suerte!";
+
---
+++ juegoj.js
@@ -0,0 +1,2 @@
+document.getElementById("c").innerHTML = "Correcto";
+document.getElementById("i").innerHTML = "Incorrecto";
